09bdae417e02e8301d513db3b312a0006bd7f6a1
fanxiaokai <fanxiaokai@starcross.tech>
2022-06-16 16:33:27 +0800

    Initial commit

---
+++ src/com/xk/leetcode/hard/_224/Solution.java
@@ -0,0 +1,48 @@
+package com.xk.leetcode.hard._224;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+/**
+ * @author 空白
+ * @date 2022/6/16
+ */
+class Solution {
+    public int calculate(String s) {
+        // 1 + 1
+        // 2 -1 + 2
+        Deque<Integer> deque = new LinkedList<>();
+        int i = 0;
+        int n = s.length();
+        int flag = 1;
+        int res = 0;
+
+        deque.push(1);
+
+        while (i < n) {
+            if (s.charAt(i) == ' ') {
+                i++;
+            } else if (s.charAt(i) == '+') {
+                flag = deque.peek();
+                i++;
+            } else if (s.charAt(i) == '-') {
+                flag = -deque.peek();
+                i++;
+            } else if (s.charAt(i) == '(') {
+                deque.push(flag);
+                i++;
+            } else if (s.charAt(i) == ')') {
+                deque.pop();
+                i++;
+            } else {
+                long num = 0;
+                while (i < n && Character.isDigit(s.charAt(i))) {
+                    num = num * 10 + s.charAt(i) - '0';
+                    i++;
+                }
+                res += flag * num;
+            }
+        }
+        return res;
+    }
+}
